88a5053168647f73ec53f8c8be4fd7494aafc285
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-11-12 11:22:27 +0530

    Multi CLIENTS

---
+++ 12-11-2024/MultiDateClient.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+
+public class MultiDateClient {
+/**
+* @param args the command line arguments
+*/
+public static void main(String args[]) throws Exception
+{
+Socket soc=new Socket(InetAddress.getLocalHost(),19999);
+BufferedReader in=new BufferedReader(
+new InputStreamReader(
+
+soc.getInputStream()
+)
+);
+System.out.println(in.readLine());
+System.out.println(in.readLine());
+}
+}
